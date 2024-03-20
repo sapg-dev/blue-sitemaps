5892c4e872c351d07783fa4489f7dcefd497ea2e
Sacha P <sacha@passebecq.ch>
2024-03-20 20:58:50 +0000

    Update updateArticles.js

--- netlify/functions/updateArticles.js
+++ netlify/functions/updateArticles.js
@@ -2,7 +2,7 @@ const { MongoClient, ObjectId } = require('mongodb');
 
 exports.handler = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
-  const MONGO_URI = "mongodb+srv://blue-sitemaps:tIRguvKmd8d7cDQa@blue.hgntyje.mongodb.net/?retryWrites=true&w=majority&appName=blue";
+  const MONGO_URI = "redacted";
 
   try {
     if (event.httpMethod !== 'PATCH') {
